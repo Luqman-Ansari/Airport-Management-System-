772552b2f8f0da90de1ab15e067d4ce51aeaf307
Luqman-Ansari <116886893+Luqman-Ansari@users.noreply.github.com>
2023-03-29 21:27:52 +0500

    Delete Q19.sql

--- Q19.sql
+++
@@ -1,7 +0,0 @@
-use airport1 
-
-select hangar.Num,  hangar.Location_, hangar.Capacity, count(airplane.HangarNum) as Planes_Stored
-from hangar 
-join airplane on airplane.HangarNum = hangar.Num
-group by hangar.Location_, hangar.Capacity, hangar.num
-
