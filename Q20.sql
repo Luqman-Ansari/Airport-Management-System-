bbcde3f0bf4a94f9a44884b60e3e7779a80f805c
Luqman-Ansari <116886893+Luqman-Ansari@users.noreply.github.com>
2023-03-29 21:28:00 +0500

    Delete Q20.sql

--- Q20.sql
+++
@@ -1,6 +0,0 @@
-use airport1 
-
-select (select p.mname from planet p where a.planetype = p.Modelnum) as Pname, count(a.planetype) as 'No. of Planes'
-from airplane a
-inner join planet p on a.PlaneType = p.modelnum
-group by a.PlaneType
