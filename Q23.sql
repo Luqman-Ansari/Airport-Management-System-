d6f90265a896a596d4c4dde0feb7f2cedf242a1b
Luqman-Ansari <116886893+Luqman-Ansari@users.noreply.github.com>
2023-03-29 21:28:32 +0500

    Delete Q23.sql

--- Q23.sql
+++
@@ -1,5 +0,0 @@
-use airport1 
-
-select COALESCE(o.CorpotateName, ' ') + COALESCE((select p.PName from Person_v p where p.SSN = o.PersonID), ' ') as Owner , count(o.AirplaneID) as Planes_owned
-from owner o
-group by o.PersonID, o.CorpotateName
